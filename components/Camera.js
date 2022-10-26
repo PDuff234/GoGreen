da122a29a4cca70b44df64ad13108f95f85609d5
PDuff234 <patrickduff234@gmail.com>
2022-10-25 22:29:31 -0400

    Camera uploads

---
+++ components/Camera.js
@@ -0,0 +1,43 @@
+import react ,{useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,Button,Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+
+const Camera = () => {
+  const [image,setImage]=useState(null)
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+      allowsEditing:true
+    });
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+
+
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden={true} />
+      {image && <Image source={{uri:image}} style={{flex:1,width:600}} />}
+      <Button title="Pick Image" onPress={pickImage}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Camera; 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,30 +1,12 @@
 import React, {useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Text, View, Image, SafeAreaView, ScrollView, StyleSheet } from "react-native";
 
-
-let dataList = []; 
-
+import Camera from "../components/Camera"
 
 const Home = () => {
-
-
-	const [isLoading, setLoading] = useState(true); 
-	const [data, setData] = useState([]); 
-	console.log(data);
-
-	var temp = JSON.stringify(data); 
-
-	useEffect(() => {
-		fetch('http://54.174.101.94:5000/getallfoodtrucks') 
-		.then((response) => response.json())
-		.then((json) =>{ setData(json)})
-		.catch((error) => console.error(error))
-		.finally(() => setLoading(false)); 
-	}, []); 
-
 return (
     <View style={{ flex: 1, padding: 24 }}>
-      
+      <Camera></Camera>
     </View>
   );
 };
