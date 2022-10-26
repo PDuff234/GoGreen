b1bb99abff573bda5308e688fe94246b43ae34ae
PDuff234 <patrickduff234@gmail.com>
2022-10-26 04:57:20 -0400

    Updated work

--- components/Camera.js
+++ components/Camera.js
@@ -1,7 +1,8 @@
-import react ,{useState} from 'react';
+import React,{useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button,Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { ButtonGroup, Card } from 'react-native-elements'; 
 
 
 const Camera = () => {
@@ -19,11 +20,34 @@ const Camera = () => {
     }
   };
 
+  const ButtonGroupModeSelection = ({ updateIndex, inputModeIndex }) => {
+    return (
+        <ButtonGroup
+            selectedTextStyle = {{ color: 'white' }}
+            selectedButtonStyle = {{backgroundColor: '#30518e'}}
+            onPress = {updateIndex}
+            selectedIndex = {inputModeIndex}
+            buttons = {['Snap Photo', 'Upload Photo']}
+            containerStyle = {{ height: 40, width: 370, margin: 0, padding: 0 }}
+        />
+    );
+  };
 
+  interface Props {
+    foundMaterials?: object[];
+  }
+  
+  interface State {
+    hasCameraPermission: boolean | undefined;
+    type: string;
+    image: string;
+    loading: boolean;
+    inputModeIndex: number;
+  }
 
   return (
     <View style={styles.container}>
-      <StatusBar hidden={true} />
+      <StatusBar style="auto" />
       {image && <Image source={{uri:image}} style={{flex:1,width:600}} />}
       <Button title="Pick Image" onPress={pickImage}/>
       <StatusBar style="auto" />
