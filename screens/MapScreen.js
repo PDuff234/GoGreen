0936b657c27a3163f2d1e760339022f45e224842
Taiga <78113881+Taiga83@users.noreply.github.com>
2022-10-26 22:33:21 -0400

    Commented out function calls

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -4,11 +4,12 @@ import { Ionicons } from "@expo/vector-icons";
 
 const Map = () => {
 return (
-	<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-		<Text style={{ color: "#006600", fontSize: 40 }}>Map Screen!</Text>
-		<Ionicons name="md-map-sharp" size={80} color="#006600" />
-		map_function()
-	</View>
+	// <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+	// 	<Text style={{ color: "#006600", fontSize: 40 }}>Map Screen!</Text>
+	// 	<Ionicons name="md-map-sharp" size={80} color="#006600" />
+	// 	map_function()
+	// </View>
+	<Text style={{ color: "#006600", fontSize: 40 }}>Map Screen!</Text>
 );
 };
 
