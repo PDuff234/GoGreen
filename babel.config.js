97e714561dc0845b7ea4bf8315ae9d32686e1dd9
Taiga <78113881+Taiga83@users.noreply.github.com>
2022-11-13 16:41:14 -0500

    Updated bable.config.js and dependencies
    Added in web dependency for react-native-reanimated
    
    Updated react-native-reanimated

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,3 @@
-/*
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
-*/
-/*
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: [
-    "react-native-reanimated/plugin",
-  ],
-};
-*/
-
 module.exports = function (api) {
   api.cache(true);
 
@@ -22,10 +5,10 @@ module.exports = function (api) {
     presets: ["babel-preset-expo"],
     env: {
       development: {
-        plugins: ["react-native-reanimated/plugin"],
+        plugins: ["@babel/plugin-proposal-export-namespace-from", "react-native-reanimated/plugin"],
       },
       production: {
-        plugins: ["react-native-reanimated/plugin", "react-native-paper/babel"],
+        plugins: ["react-native-paper/babel", "@babel/plugin-proposal-export-namespace-from", "react-native-reanimated/plugin"],
       },
     },
   };
