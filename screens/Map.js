2a719d246e54372a6f0532d325c88cefcc914a0f
Taiga <78113881+Taiga83@users.noreply.github.com>
2022-11-17 15:42:36 -0500

    Refactored map screen

--- screens/Map.js
+++ screens/Map.js
@@ -1,58 +1,92 @@
-import React from 'react';
-import { GoogleMap, Autocomplete,DirectionsRenderer, Marker, InfoWindow, useJsApiLoader } from '@react-google-maps/api';
-import {useRef,useState} from 'react';
+import React, {useEffect,useState} from 'react';
+import { Text, View, ActivityIndicator, StyleSheet} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import * as Location from 'expo-location';
 
-const containerStyle = {
-  width: '100%',
-  height: '100%'
-};
+import { MATERIALS } from '../constants';
+import { primaryColor } from '../styles/constants';
+import Results from '../components/Results';
 
-const location = { lat: 28.05873, lng: -82.41564}
-
-function MyComponent() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyAU5WKtwKIV2PB9DpAfv9zDyG1-mWKskkQ",
-    libraries: ['places'],
-  })
-
-  const [map, setMap] = React.useState(null)
-  const [directionsResponse, setDirectionsResponse] = useState(null)
-  const [distance, setDistance] = useState('')
-  const [duration, setDuration] = useState('')
-
-  const originRef ={lat: 28.05873, lng: -82.41564}
-  const destinationRef ={lat: 28.05506, lng: -82.40021}
-
-  async function calculateRoute() {
-    if (originRef.current.value === '' || destiantionRef.current.value === '') {
-      return
-    }
-    // eslint-disable-next-line no-undef
-    const directionsService = new google.maps.DirectionsService()
-    const results = await directionsService.route({
-      origin: originRef,
-      destination: destinationRef,
-      // eslint-disable-next-line no-undef
-      travelMode: google.maps.TravelMode.DRIVING,
-    })
-    setDirectionsResponse(results)
-    setDistance(results.routes[0].legs[0].distance.text)
-    setDuration(results.routes[0].legs[0].duration.text)
-  }
-
-  return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={location}
-        zoom={18}
-       
-      >
-        <Marker position={location} />
-        <Marker position={{lat: 28.05506, lng: -82.40021}} />
-        <Marker position={{lat: 28.07415, lng: -82.43824}} />
-      </GoogleMap>
-  ) : <></>
+const searchParams = {
+  maxDistance: 25,
+  maxResults: 20,
 }
 
-export default React.memo(MyComponent)
+const Map = ({ label }) => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, seterrorMsg] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getPermissions(){
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted'){
+        seterrorMsg('Please allow location permission to use this feature');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setLoading(false);
+
+      searchParams.geolocation = { 
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+       }
+      
+    }
+    getPermissions();
+  }, []);
+
+  if (errorMsg){
+    return (
+      <View style={styles.mapContainer}>
+        <Text>{errorMsg}</Text>
+      </View>
+    )
+  }
+
+
+  switch(label){
+    case "Government_Dropoff1paper":
+      searchParams.material_id = MATERIALS.paperCup;   
+    case "Retail_Grocery1foam":
+      searchParams.material_id = MATERIALS.foam;
+    case "Retail_Grocery1plastic":
+      searchParams.material_id = MATERIALS.plasticBag;
+    case "Retail_Hardware1cfl":
+      searchParams.material_id = MATERIALS.cfl;
+  }
+
+
+  return (
+    <View style={styles.mapContainer}>
+      { isLoading ? 
+        <ActivityIndicator size="large" color={primaryColor}/> :
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={{flex: 1}}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+        >
+          <Results searchParams={searchParams}></Results>      
+        </MapView> 
+      }
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  mapContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  }
+})
+
+export default Map
