9ff792a9705c895e7fdff94caccc5a6bb33b3366
Taiga <78113881+Taiga83@users.noreply.github.com>
2022-11-29 19:11:08 -0500

    Included provider for item context

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { RootNavigator } from './navigation/RootNavigator';
 import { AuthenticatedUserProvider } from './providers';
+import { ItemProvider } from "./context/ItemContext";
 
 export default function App() {
 	return (
 		<AuthenticatedUserProvider>
-			<SafeAreaProvider>
-		  		<RootNavigator />
-			</SafeAreaProvider>
-	  	</AuthenticatedUserProvider>
+				<SafeAreaProvider>
+						<ItemProvider>
+							<RootNavigator />
+						</ItemProvider>
+				</SafeAreaProvider>
+			</AuthenticatedUserProvider>
 	);
 }
